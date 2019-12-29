12e111616d5d3beb67799a18f8fd7e2abd437fe7
valeriy-kabisov <valeriy.kabisov@novapulsar.com>
2019-12-29 16:23:48 +0300

    initial

---
+++ app/Models/DownloaderJob.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DownloaderJob extends Model
+{
+    protected $table = 'jobs';
+    protected $primaryKey = 'id';
+}
---
+++ database/migrations/2019_12_29_125030_create_job_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('status', 128);
+            $table->text('resource');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
